bd875bb5b59c3a949c8b340fd563e1f426aecefe
DevThienVKU <bxthien.18it3@vku.udn.vn>
2021-08-23 18:30:27 +0700

    Version 2. Image and Video

--- api/views.py
+++ api/views.py
@@ -5,49 +5,49 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-class ImageViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class ImageViewSet(viewsets.ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#
+#     def post(self, request, *args, **kwargs):
+#         cover = request.data['cover']
+#         title = request.data['title']
+#         Book.objects.create(title=title, cover=cover)
+#         return Response({'message': 'Book created'}, status=200)
 
-    def post(self, request, *args, **kwargs):
-        cover = request.data['cover']
-        title = request.data['title']
-        Book.objects.create(title=title, cover=cover)
-        return Response({'message': 'Book created'}, status=200)
 
+class ImageViewSet(viewsets.ViewSet):
+    def list(self,request):
+        book = Book.objects.all()
+        serializer = BookSerializer(book, many=True)
+        return Response(serializer.data)
 
-# class ImageViewSet(viewsets.ViewSet):
-#     def list(self,request):
-#         book = Book.objects.all()
-#         serializer = BookSerializer(book, many=True)
-#         return Response(serializer.data)
-#
-#     def create(self, request):
-#         serializer = BookSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def retrieve(self, request, pk= None):
-#         queryset = Book.objects.all()
-#         book = get_object_or_404(queryset, pk= pk)
-#         serializer = BookSerializer(book)
-#         return Response(serializer.data)
-#
-#     def update(self, request, pk = None):
-#         book = BookSerializer.objects.get(pk= pk)
-#
-#         serializer = BookSerializer(book, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk= None):
-#         book = Book.objects.get(pk=pk)
-#         book.delete()
-#         return Response(status = status.HTTP_204_NO_CONTENT)
+    def create(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk= None):
+        queryset = Book.objects.all()
+        book = get_object_or_404(queryset, pk= pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def update(self, request, pk = None):
+        book = BookSerializer.objects.get(pk= pk)
+
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk= None):
+        book = Book.objects.get(pk=pk)
+        book.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
 class VideoViewSet(viewsets.ViewSet):
     def list(self, request):
